6468e1f7f51a8b1522878964cb8663444a392f4d
pankajrao55 <pankajrao5577@gmail.com>
2026-04-14 11:39:17 +0530

    version 1

--- src/client.java
+++ src/client.java
@@ -2,9 +2,6 @@ public class client {
 
     public static void main(String[] args) {
 
-        System.out.println("pankaj");
-        System.out.println("kuch bhi");
-
-        System.out.println("xyz");
+        System.out.println("version 1");
     }
 }
